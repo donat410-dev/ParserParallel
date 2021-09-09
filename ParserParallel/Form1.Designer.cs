ca6df1e285dddf717701129add1039074651ca36
Dmitry Druzhin <donat410@gmail.com>
2021-09-09 22:15:58 +0300

    Add project files.

---
+++ ParserParallel/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace ParserParallel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.Url = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.Deep = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.StartButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.ShowButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel3 = new System.Windows.Forms.ToolStripLabel();
+            this.ProgressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.Url,
+            this.toolStripSeparator1,
+            this.toolStripLabel2,
+            this.Deep,
+            this.toolStripSeparator2,
+            this.StartButton,
+            this.toolStripSeparator4,
+            this.ShowButton,
+            this.toolStripSeparator3,
+            this.toolStripLabel3,
+            this.ProgressBar});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(868, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(31, 22);
+            this.toolStripLabel1.Text = "URL:";
+            // 
+            // Url
+            // 
+            this.Url.Name = "Url";
+            this.Url.Size = new System.Drawing.Size(100, 25);
+            this.Url.Text = "https://mail.ru";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(37, 22);
+            this.toolStripLabel2.Text = "Deep:";
+            // 
+            // Deep
+            // 
+            this.Deep.Name = "Deep";
+            this.Deep.Size = new System.Drawing.Size(25, 25);
+            this.Deep.Text = "0";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // StartButton
+            // 
+            this.StartButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.StartButton.Image = ((System.Drawing.Image)(resources.GetObject("StartButton.Image")));
+            this.StartButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(35, 22);
+            this.StartButton.Text = "Start";
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 25);
+            // 
+            // ShowButton
+            // 
+            this.ShowButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.ShowButton.Image = ((System.Drawing.Image)(resources.GetObject("ShowButton.Image")));
+            this.ShowButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ShowButton.Name = "ShowButton";
+            this.ShowButton.Size = new System.Drawing.Size(40, 22);
+            this.ShowButton.Text = "Show";
+            this.ShowButton.Click += new System.EventHandler(this.ShowButton_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel3
+            // 
+            this.toolStripLabel3.Name = "toolStripLabel3";
+            this.toolStripLabel3.Size = new System.Drawing.Size(52, 22);
+            this.toolStripLabel3.Text = "Progress";
+            // 
+            // ProgressBar
+            // 
+            this.ProgressBar.Name = "ProgressBar";
+            this.ProgressBar.Size = new System.Drawing.Size(300, 22);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 25);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(868, 425);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "ParserParallel";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox Url;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripTextBox Deep;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton StartButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripButton ShowButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel3;
+        private System.Windows.Forms.ToolStripProgressBar ProgressBar;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ ParserParallel/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParserParallel
+{
+    public partial class Form1 : Form
+    {
+        private readonly Stopwatch _stopwatch = new();
+        private readonly ConcurrentBag<string> _visitedUrl = new();
+        private readonly ConcurrentBag<string> _errorUrl = new();
+        private readonly ConcurrentBag<string> _readyToVisit = new();
+        private readonly HttpClient _httpClient = new();
+        private const string HRefPattern = @"href\s*=\s*(?:[""'](?<1>[^""']*)[""']|(?<1>\S+))";
+        private readonly string[] _fileType = { ".js", ".pdf", ".jpg", ".png", ".gif", ".css", ".jpeg" };
+        private int _deepWalk;
+        private Match _m;
+        private Regex _fileTypePattern;
+        private ConcurrentBag<string> _tempBag;
+        private string _html;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void StartButton_Click(object? sender, EventArgs e)
+        {
+            StartButton.Enabled = false;
+            ShowButton.Enabled = false;
+            Url.Enabled = false;
+            Deep.Enabled = false;
+
+            _visitedUrl.Clear();
+            _errorUrl.Clear();
+            _readyToVisit.Clear();
+            richTextBox1.Clear();
+
+            if (!int.TryParse(Deep.Text, out _deepWalk))
+                return;
+
+            _readyToVisit.Add(Url.Text);
+            _stopwatch.Restart();
+            await Task.Run(RunParser);
+            _stopwatch.Stop();
+            richTextBox1.AppendText($"Было потрачено {_stopwatch.ElapsedMilliseconds / 1000.0} секунд(ы)\n\n");
+            ShowButton.Enabled = true;
+            StartButton.Enabled = true;
+            Deep.Enabled = true;
+            Url.Enabled = true;
+            richTextBox1.AppendText($"Ссылок с непрошедшими запросами: {_errorUrl.Count}\n");
+            richTextBox1.AppendText($"Посещенные ссылки: {_visitedUrl.Count}\n");
+            richTextBox1.AppendText($"Ссылrи готовые к посещению: {_readyToVisit.Count}\n");
+        }
+
+        private void RunParser()
+        {
+            while (true)
+            {
+                _tempBag = new ConcurrentBag<string>(_readyToVisit);
+                _readyToVisit.Clear();
+
+                // устанавливаем число ссылок на данном уровне глубины как максимум для прогрессбара
+                Invoke(new Action(() => ProgressBar.Maximum = _tempBag.Count));
+
+                var tasks = new ConcurrentBag<Task>();
+                Parallel.ForEach(_tempBag, s => tasks.Add(GetUrlAsync(s)));
+                Task.WaitAll(tasks.ToArray());
+
+                _tempBag.Clear();
+
+                // если глубина больше текущей то повторям обход
+                if (_deepWalk > 0)
+                {
+                    _deepWalk--;
+                    continue;
+                }
+
+                _tempBag.Clear();
+                break;
+            }
+        }
+
+        private async Task GetUrlAsync(string url)
+        {
+            try
+            {
+                _html = await _httpClient.GetStringAsync(url);
+
+                _visitedUrl.Add(url);
+
+                // ищем ссылки
+                _m = Regex.Match(_html, HRefPattern,
+                    RegexOptions.IgnoreCase | RegexOptions.Compiled,
+                    TimeSpan.FromSeconds(1));
+
+                while (_m.Success)
+                {
+                    var tmpUrl = $"{_m.Groups[1]}";
+
+                    // различные ограничения
+                    if (tmpUrl.IndexOf("http", StringComparison.Ordinal) == 0)
+                        if (!_fileTypePattern.IsMatch(tmpUrl))
+                            if (!_visitedUrl.Contains(tmpUrl) && !_readyToVisit.Contains(tmpUrl))
+                                _readyToVisit.Add(tmpUrl);
+
+                    _m = _m.NextMatch();
+                }
+            }
+            catch (Exception e)
+            {
+                _errorUrl.Add(url);
+            }
+            finally
+            {
+                Invoke(new Action(() => ProgressBar.PerformStep()));
+            }
+        }
+
+        private void ShowInfo()
+        {
+            Invoke(new Action(() =>
+            {
+                if (_errorUrl.Count > 0)
+                {
+                    richTextBox1.AppendText("\nerrorUrl:\n");
+                    foreach (var item in _errorUrl)
+                        richTextBox1.AppendText($"->{item}\n");
+                }
+
+                richTextBox1.AppendText("\nvisitedUrl:\n");
+                foreach (var item in _visitedUrl)
+                    richTextBox1.AppendText($"->{item}\n");
+
+                richTextBox1.AppendText("\nreadyToVisit:\n");
+                foreach (var item in _readyToVisit)
+                    richTextBox1.AppendText($"->{item}\n");
+            }));
+        }
+
+        private async void ShowButton_Click(object? sender, EventArgs e)
+        {
+            StartButton.Enabled = false;
+            ShowButton.Enabled = false;
+            Url.Enabled = false;
+            Deep.Enabled = false;
+            await Task.Run(ShowInfo);
+            StartButton.Enabled = true;
+            ShowButton.Enabled = true;
+            Url.Enabled = true;
+            Deep.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // гененрируем из массива регулярное выражение для исключения ссылок на файлы
+            _fileTypePattern = new Regex(string.Join("|", _fileType.Select(Regex.Escape)));
+        }
+    }
+}
