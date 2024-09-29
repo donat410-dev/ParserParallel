e34c501234631e47121cfe5caa72ea779f5deca7
Dmitry Druzhin <53137759+donat410-dev@users.noreply.github.com>
2024-09-29 21:33:29 +0300

    Обновил NET, вынес парсер в отдельный класс

--- Program.cs
+++ Program.cs
@@ -1,35 +1,20 @@
-﻿using System.Collections.Concurrent;
-using System.Text.RegularExpressions;
-
-const string hrefPattern = @"href\s*=\s*(?:[""'](?<1>[^""']*)[""']|(?<1>[^>\s]+))";
-
-ConcurrentBag<string> resourceUrl = new();
-ConcurrentBag<string> readyToVisitUrl = new();
-ConcurrentBag<string> errorUrl = new();
-ConcurrentBag<string> outerUrl = new();
-List<Task> tempUrl = new();
-HttpClient httpClient = new();
-string domain;
-string[] fileType = { ".js", ".pdf", ".jpg", ".png", ".gif", ".css", ".jpeg", ".xml" };
-
-var fileTypePattern = new Regex(string.Join("|", fileType.Select(Regex.Escape)));
+﻿using ParserParallel;
 
 Console.Write("Введите URL: ");
 var urlConsole = Console.ReadLine();
-if (Uri.IsWellFormedUriString(urlConsole, UriKind.Absolute))
-{
-    domain = new Regex(@"(?<=\/)[a-z0-9\-\.]+\.[0-9a-z]+(?=\/|$)").Matches(urlConsole)[0].Value;
-    domain = domain.Replace("www.", "");
-    readyToVisitUrl.Add(urlConsole);
-}
-else
-{
-    Console.WriteLine("Вы не ввели URL!");
-    Console.ReadKey();
-    return;
-}
 
-RunParser();
+while (!Uri.IsWellFormedUriString(urlConsole, UriKind.Absolute))
+{
+    Console.Write("Введите корректный URL: ");
+    urlConsole = Console.ReadLine();
+}
+var parser = new UrlParser(urlConsole);
+
+await parser.RunAsync();
+
+var resourceUrl = parser.ResourceUrls;
+var errorUrl = parser.ErrorUrls;
+var outerUrl = parser.OuterUrls;
 
 Console.WriteLine($"Страницы ресурса: {resourceUrl.Count}");
 Console.WriteLine($"Ссылки на внешние ресурсы: {outerUrl.Count}");
@@ -39,76 +24,14 @@ Console.Write("Отобразить полученные ссылки? (y/n): ")
 
 if (Console.ReadLine()!.Contains('y'))
 {
-    ShowInfo();
-    Console.ReadKey();
-}
-
-void RunParser()
-{
-    while (!readyToVisitUrl.IsEmpty)
-    {
-        while (readyToVisitUrl.TryTake(out var url))
-        {
-            tempUrl.Add(Task.Run(() => GetUrlAsync(url)));
-        }
-
-        Task.WaitAll(tempUrl.ToArray());
-        tempUrl.Clear();
-    }
-}
-
-async Task GetUrlAsync(string url)
-{
-    try
-    {
-        var html = await httpClient.GetStringAsync(url);
-
-        resourceUrl.Add(url);
-        // ищем ссылки
-        var m = Regex.Match(html, hrefPattern,
-            RegexOptions.Compiled,
-            TimeSpan.FromSeconds(5));
-
-        while (m.Success)
-        {
-            var tmpUrl = $"{m.Groups[1]}";
-            if (tmpUrl[0] == '/') tmpUrl = "https://" + domain + tmpUrl;
-
-            // различные ограничения
-            if (Uri.IsWellFormedUriString(tmpUrl, UriKind.Absolute))
-            {
-                if (!fileTypePattern.IsMatch(tmpUrl))
-                {
-                    if (!resourceUrl.Contains(tmpUrl) && !readyToVisitUrl.Contains(tmpUrl) &&
-                        !outerUrl.Contains(tmpUrl))
-                    {
-                        if (tmpUrl.Contains(domain)) readyToVisitUrl.Add(tmpUrl);
-                        else outerUrl.Add(tmpUrl);
-                    }
-                }
-            }
-
-            m = m.NextMatch();
-        }
-    }
-    catch (Exception e)
-    {
-        errorUrl.Add(url);
-    }
-}
-
-void ShowInfo()
-{
     Console.WriteLine("Все страницы ресурса:");
-    foreach (var item in resourceUrl)
-        Console.WriteLine($"->{item}");
-
+    foreach (var item in resourceUrl) Console.WriteLine($"->{item}");
+    
     Console.WriteLine("Внешние URL:");
-    foreach (var item in outerUrl)
-        Console.WriteLine($"->{item}");
-
-    if (errorUrl.IsEmpty) return;
+    foreach (var item in outerUrl) Console.WriteLine($"->{item}");
+    
     Console.WriteLine("Непрошедшие запросы:");
-    foreach (var item in errorUrl)
-        Console.WriteLine($"->{item}");
+    foreach (var item in errorUrl) Console.WriteLine($"->{item}");
 }
+
+Console.ReadKey();
---
+++ UrlParser.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections.Concurrent;
+using System.Text.RegularExpressions;
+
+namespace ParserParallel;
+
+public class UrlParser
+{
+    private static readonly string[] FileTypes = [
+            "js", "css", "png", "jpg", "jpeg", 
+            "gif", "svg", "webp", "mp4", "webm", 
+            "ogg", "mp3", "wav", "pdf", "ico", 
+            "woff", "woff2", "ttf", "otf"];
+
+    private static readonly Regex HrefPattern = new(
+        """\b((http|https|ftp):\/\/[-\w@:%_\+.~#?&//=]+|www\.[-\w@:%_\+.~#?&//=]+(\.[a-z]{2,6})?)""", 
+        RegexOptions.Compiled | RegexOptions.IgnoreCase,
+        TimeSpan.FromSeconds(10)
+    );
+    
+    private static readonly Regex FileTypePattern = new(
+        $@"\.({string.Join("|", FileTypes.Select(Regex.Escape))})$", 
+        RegexOptions.Compiled | RegexOptions.IgnoreCase,
+        TimeSpan.FromSeconds(3)
+    );
+    
+    private readonly HttpClient _httpClient;
+    
+    private readonly ConcurrentDictionary<string, bool> _resourceUrls = [];
+    private readonly ConcurrentDictionary<string, bool> _readyToVisitUrls = [];
+    private readonly ConcurrentDictionary<string, bool> _errorUrls = [];
+    private readonly ConcurrentDictionary<string, bool> _outerUrls = [];
+
+    private readonly string _domain;
+
+    public IReadOnlyCollection<string> ResourceUrls => [.._resourceUrls.Keys];
+    public IReadOnlyCollection<string> OuterUrls => [.._outerUrls.Keys];
+    public IReadOnlyCollection<string> ErrorUrls => [.._errorUrls.Keys];
+
+    public UrlParser(string resourceUrl, HttpClient httpClient = null)
+    {
+        _httpClient = httpClient ?? new HttpClient();
+        _readyToVisitUrls.TryAdd(resourceUrl, true);
+        _domain = new Uri(resourceUrl).Host;
+    }
+
+    public async Task RunAsync(int maxDegreeOfParallelism = 100, CancellationToken cancellationToken = default)
+    {
+        while (!_readyToVisitUrls.IsEmpty)
+        {
+            var urlsToVisit = _readyToVisitUrls.ToArray();
+            _readyToVisitUrls.Clear();
+
+            var options = new ParallelOptions
+            {
+                MaxDegreeOfParallelism = maxDegreeOfParallelism,
+                CancellationToken = cancellationToken
+            };
+            await Parallel.ForEachAsync(urlsToVisit, options, async (url, ctx) => await GetUrlAsync(url.Key, ctx));
+        }
+    }
+
+    private async Task GetUrlAsync(string url, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            if (!_resourceUrls.TryAdd(url, true)) return;
+
+            var html = await _httpClient.GetStringAsync(url, cancellationToken);
+            ProcessLinks(html);
+        }
+        catch
+        {
+            _errorUrls.TryAdd(url, true);
+        }
+    }
+    
+    private bool TryGetDomain(string url, out string domain)
+    {
+        domain = null;
+        try
+        {
+            var uri = new Uri(url);
+            domain = uri.Host;
+            return true;
+        }
+        catch
+        {
+            _errorUrls.TryAdd(url, true);
+            return false;
+        }
+    }
+    
+    private void ProcessLinks(string html)
+    {
+        foreach (Match match in HrefPattern.Matches(html))
+        {
+            var tmpUrl = match.Groups[1].Value;
+            
+            if (FileTypePattern.IsMatch(tmpUrl)) continue;
+            if (!TryGetDomain(tmpUrl, out var tmpDomain)) continue;
+            
+            if (tmpDomain == _domain)
+            {
+                _readyToVisitUrls.TryAdd(tmpUrl, true);
+            }
+            else
+            {
+                _outerUrls.TryAdd(tmpUrl, true);
+            }
+        }
+    }
+}
